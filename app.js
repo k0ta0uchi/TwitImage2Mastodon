f0d5e8c63878216dc777bfe0aff9da71bc9a91b3
k0ta0uchi <k0ta0uchi@gmail.com>
2017-11-01 23:26:52 +0900

    added video support.

--- app.js
+++ app.js
@@ -38,12 +38,26 @@ var stream = T.stream('user', [user])
 
 stream.on('tweet', (tweet) => {
   if (tweet.user.screen_name === user && tweet.text.indexOf('#NintendoSwitch') !== -1) { 
+    // delete twitter url
+    var text = tweet.text
+    text = text.replace(/https:\/\/t\.co\/.*$/, '')
+    
     // download image first.
     var media = tweet.extended_entities.media[0]
-    console.log(media.media_url_https)
-    var tempPath = './temp.jpg'
+    let ext = '.jpg'
+    let media_url = media.media_url_https
+    if (undefined !== media.video_info) {
+      ext = '.mp4'
+      var videoes = media.video_info.variants
+      for (let i = 0; i < videoes.length; i++) {
+        if (videoes[i].content_type === 'video/mp4')
+          media_url = videoes[i].url
+      }
+    }
+    console.log(media_url)
+    var tempPath = './temp' + ext
     var image = fs.createWriteStream(tempPath)
-    https.get(media.media_url_https, (_res) => {
+    https.get(media_url, (_res) => {
       _res.pipe(image)
     })
 
@@ -55,10 +69,10 @@ stream.on('tweet', (tweet) => {
         const id = resp.data.id
 
         // post status using media id.
-        M.post('statuses', { status: tweet.text, media_ids: [id] }).then(resp => {
+        M.post('statuses', { status: text, media_ids: [id] }).then(resp => {
           console.log('status posted successfully.')
           fs.unlinkSync(tempPath)
-          res.sendStatus(200)
+          //res.sendStatus(200)
         })
       })
     })
